d17894aa4cba4030d659759a22f8338e02704615
cashout01 <53282533+cashout01@users.noreply.github.com>
2019-12-09 11:25:40 -0500

    Add files via upload

--- HomeController.java
+++ HomeController.java
@@ -1,12 +1,14 @@
-package springboot101.demo;
+package com.bysw.springboot102;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequeatMapping("/")
-    public String index() {
-        return "index";
+
+    @RequestMapping("/")
+    public String homePage(Model model) {
+        model.addattribute("myvar", "Say hello to the people");
+        return "homeTemplate";
     }
 }
